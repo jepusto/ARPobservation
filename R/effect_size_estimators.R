a85640ff277a25438317a140de6ebe8727321a59
jepusto <jepusto@gmail.com>
2021-04-30 11:25:29 -0500

    Edited example for PIR_MOM() to use fewer bootstraps.

--- R/effect_size_estimators.R
+++ R/effect_size_estimators.R
@@ -681,9 +681,14 @@ PIRbootstrappair_old <- function(nObs, phi, zeta, active, rest, K, iterations, a
 #' # Estimate prevalence and incidence ratios for Carl from the Moes dataset
 #' data(Moes)
 #' with(subset(Moes, Case == "Carl"),
-#' PIR_MOM(PIR = outcome, phase = Phase, intervals = intervals,
-#' interval_length = (active_length + rest_length), rest_length = rest_length,
-#' base_level = "No Choice", seed = 149568373))
+#'   PIR_MOM(PIR = outcome,
+#'           phase = Phase,
+#'           intervals = intervals,
+#'           interval_length = (active_length + rest_length),
+#'           rest_length = rest_length,
+#'           base_level = "No Choice",
+#'           Bootstraps = 200,
+#'           seed = 149568373))
 #'
 #' @author Daniel Swan <dswan@@utexas.edu>
 #' @export
